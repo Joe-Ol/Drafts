57bfbaafc23ac9b5b55bf5d59aebfd4583b2ded7
Joe <64926283+JoeOlang@users.noreply.github.com>
2021-04-17 22:12:16 +0300

    Update v1.py

--- QLearning/WarehouseBot/v1.py
+++ QLearning/WarehouseBot/v1.py
@@ -1,18 +1,14 @@
-"""
-
-Environment = States, Action and Rewards
-
-"""
-
+# Libraries
 import numpy as np
 
 # Define environment
-
 environment_rows = 11
-environment_cols = 11
+environment_columns = 11
 
+
+# Create a 3D numpy array to hold the current Q-values for each state and action pair: Q(s, a) 
+# The value of each (state, action) pair is initialized to 0.
 q_values = np.zeros((environment_rows, environment_cols, 4))
-
 actions = ['up', 'right', 'down', 'left']
 
 rewards = np.full((environment_cols, environment_rows), -100.)
@@ -34,7 +30,7 @@ paths[9] = [i for i in range(11)]
 
 for row_index in range(1, 10):
     for col_index in paths[row_index]:
-        rewards[row_index, col_index] = 1.
+        rewards[row_index, col_index] = -1.
 
 # for row in rewards:
     # print(row)
@@ -52,12 +48,12 @@ def is_terminal_state(current_row_index, current_column_index):
 def get_starting_location():
     # get a random row & column
     current_row_index = np.random.randint(environment_rows)
-    current_column_index = np.random.randint(environment_rows)
+    current_column_index = np.random.randint(environment_columns)
 
     # continue choosing random points until non terminal state is found i.e. until a white square is chosen
     while is_terminal_state(current_column_index, current_column_index):
         current_row_index = np.random.randint(environment_rows)
-        current_column_index = np.random.randint(environment_rows)
+        current_column_index = np.random.randint(environment_columns)
 
     return current_row_index, current_column_index
 
@@ -80,14 +76,12 @@ def get_next_location(current_row_index, current_col_index, action_index):
     new_col_index = current_col_index
     if actions[action_index] == 'up' and current_row_index > 0:
         new_row_index -= 1
-    elif actions[action_index] == 'right' and current_col_index < environment_cols - 1:
-        new_row_index -= 1
+    elif actions[action_index] == 'right' and current_col_index < environment_columns - 1:
+        new_row_index += 1
     elif actions[action_index] == 'down' and current_row_index < environment_rows - 1:
-        new_row_index -= 1
+        new_row_index += 1
     elif actions[action_index] == 'left' and current_col_index > 0:
         new_row_index -= 1
-    else:
-        new_col_index -= 1
     return new_row_index, new_col_index
 
 
@@ -102,7 +96,7 @@ def get_shortest_path(start_row_index, start_column_index):
         shortest_path.append([current_row_index, current_col_index])
 
         while not is_terminal_state(current_row_index, current_col_index):
-            action_index = get_next_action(current_row_index, current_col_index, 1)
+            action_index = get_next_action(current_row_index, current_col_index, 1.)
             current_row_index, current_col_index = get_next_location(current_row_index, current_col_index, action_index)
             shortest_path.append([current_row_index, current_col_index])
         return shortest_path
@@ -114,7 +108,7 @@ discount_factor = 0.9   # the discount factor for future rewards
 learning_rate = 0.9
 
 # run through 100 training episodes
-for episode in range(3):
+for episode in range(100):
     # get starting location for this episode
     row_index, col_index = get_starting_location()
 
